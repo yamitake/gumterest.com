7e79744c45abf334e8044bd49762b1eae51481d0
jun <take.yapr@gmail.com>
2012-02-25 04:12:02 +0900

    image load not good.

--- init.js
+++ init.js
@@ -1,36 +1,33 @@
-$(function(){
-  $('#target').fluentgallery();
-  
+$(function() {
+  fluentgallery = $('#target').fluentgallery();
+
   $.ajax({
-  url : 'http://api.flickr.com/services/feeds/photos_public.gne',
-  data : {
-    tags : 'cat',
-    tagmode : 'any',
-    format : 'json'
-  },
-  dataType : 'jsonp',
-  jsonp : 'jsoncallback',
-  complete : function(){
-    console.log("complate");
-  },
-  success : function(data, status){
-    console.log(data);
-    length = data.items.length;
-    for(var i = 0; i < length; i++){
-      var div = $( "#tileTemplate" ).tmpl( data.items[i] );
-      console.log(div);
-      $('#target').fluentgallery({add:div.html() , reset :false });
+    url : 'http://search.twitter.com/search.json',
+    data : {
+      q : 'on gumroad',
+      rpp : 100 ,
+      format : 'json'
+    },
+    dataType : 'jsonp',
+    jsonp : 'callback' ,
+    complete : function() {
+      console.log("complate");
+    },
+    success : function(data, status) {
+      console.log(status);
+      console.log(data);
+       length = data.results.length;
+       for(var i = 0; i < length; i++) {
+         var div = $("#tileTemplate").tmpl(data.results[i]);
+         console.log("logggg");
+         fluentgallery.append(div);
+         //$('#target').fluentgallery({add:div , reset:false});
+       }
+    },
+    beforeSend : function() {
+    },
+    error : function() {
     }
-  },
-  beforeSend : function(){
-    console.log("beforeSend");
-  },
-  error : function(){
-    console.log("monstar.fm");
-  }
-});
-  
-  function getCapture(url){
-    return 'http://capture.heartrails.com/200x200/free?' + url;
-  }
+  });
+
 });
