7359c1bde1c05dddd03ab600ddaae4c6dd12c19d
jun <take.yapr@gmail.com>
2012-02-27 00:50:51 +0900

    barstyle.

--- init.js
+++ init.js
@@ -15,8 +15,10 @@ $(function() {
     success : function(data, status) {
        length = data.results.length;
        for(var i = 0; i < length; i++) {
+         data.results[i].download_url = "http://iwatter.net";
          var div = $("#tileTemplate").tmpl(data.results[i]);
-         fluentgallery.append(div);
+         //fluentgallery.append(div);
+         $('#target').fluentgallery({add:div});
        }
     },
     beforeSend : function() {
