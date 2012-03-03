6bc722f3e881163e42e60071a762c2355086e63b
jun <take.yapr@gmail.com>
2012-03-03 18:12:13 +0900

    ドメインがなんかおかしくなった。

--- init.js
+++ init.js
@@ -15,7 +15,10 @@ $(function() {
     success : function(data, status) {
        length = data.results.length;
        for(var i = 0; i < length; i++) {
-         data.results[i].download_url = "http://iwatter.net";
+         console.log(data);
+         var obj = createTweetObj(data.results[i].text);
+         data.results[i].download_url = obj.download_url;
+         data.results[i].text = obj.text;
          var div = $("#tileTemplate").tmpl(data.results[i]);
          //fluentgallery.append(div);
          $('#target').fluentgallery({add:div});
@@ -26,5 +29,17 @@ $(function() {
     error : function() {
     }
   });
+  
+  function createTweetObj(str){
+    var download_url = "";
+    var text = str.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
+        download_url = url;
+        return '<a target="_blank" href="'+url+'">'+url+'</a>';
+      }).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
+        return  reply.charAt(0)+'<a target="_blank" href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
+      });
+    
+    return {text:text , download_url:download_url};
+  }
 
 });
